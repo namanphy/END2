5f9e0eaebd492cb47b9aa2f0889777b2b89de7b9
namanphy <namanphy@gmail.com>
2021-06-03 19:57:24 +0530

    adam added

--- S5/trainer/optimizer.py
+++ S5/trainer/optimizer.py
@@ -6,6 +6,10 @@ def sgd_optimizer(model, lr=0.01, l2_factor=0, momentum=0.9):
     return optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=l2_factor)
 
 
+def adam_optimizer(model, lr=0.01):
+    return optim.Adam(model.parameters(), lr=lr)
+
+
 def step_lr_scheduler(optimizer, step_size=None, gamma=0.15):
     if step_size is None:
         raise Exception('step size value must be provided with valid integer')
